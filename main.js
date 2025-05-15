b34cafad5f600c2657135b69951f5936fdc54a0b
jhosoan <jhosoancordeiro17@gmail.com>
2025-05-14 22:59:02 -0400

    telas criadas

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
+console.log(" #  *Arquitetura da telas")
 const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 430
   })
 
   win.loadFile('./views/index.html')
 }
-
+// janela reconhecido
+const reco = () => {
+  const about = new BrowserWindow({
+    width:1200,
+    height:150,
+    autoHideMenuBar:true,
+    resizable:false,
+    
+  })
+  about.loadFile('./views/reconhecido.html')
+}
 app.whenReady().then(() => {
   createWindow()
+  reco()
 })
